ab9226d14e0bee1fddc466a38340b422a19c27e6
unknown <ayang@costmldev6.gencos.com>
2015-08-20 02:08:57 -0700

    //Redefine the console.log function in your script to disable log output.

--- Console.log/ConsoleLogSeed.js
+++ Console.log/ConsoleLogSeed.js
@@ -8,3 +8,39 @@ console.warn( myvar, "warn!");
 console.error(myvar, "error!");
 
 var b = "abc";
+
+
+
+var logger = function()
+{
+    var oldConsoleLog = null;
+    var pub = {};
+
+    pub.enableLogger =  function enableLogger()
+    {
+        if(oldConsoleLog == null)
+            return;
+
+        window['console']['log'] = oldConsoleLog;
+    };
+
+    pub.disableLogger = function disableLogger()
+    {
+        oldConsoleLog = console.log;
+        window['console']['log'] = function() {};
+    };
+
+    return pub;
+}();
+
+
+var logsTest=  function() {
+    console.log('hello');
+
+    logger.disableLogger();
+    console.log('hi', 'hiya');
+    console.log('this wont show up in console');
+
+    logger.enableLogger();
+    console.log('This will show up!');
+}();
