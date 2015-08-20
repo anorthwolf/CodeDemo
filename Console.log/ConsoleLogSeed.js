bd0647940c272f00248771bc145ae35efdc20954
unknown <ayang@costmldev6.gencos.com>
2015-08-20 01:11:11 -0700

    it's for using gulp-strip-debug  to strip the "console.log" code in JS Strip console and debugger statements from JavaScript code

---
+++ Console.log/ConsoleLogSeed.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ayang on 8/20/2015.
+ */
+var myvar = 1;
+console.log(  myvar, "log!");
+console.info( myvar, "info!");
+console.warn( myvar, "warn!");
+console.error(myvar, "error!");
+
+var b = "abc";
---
+++ Console.log/dist/ConsoleLogSeed.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ayang on 8/20/2015.
+ */
+var myvar = 1;
+void 0;
+void 0;
+void 0;
+void 0;
+
+var b = "abc";
